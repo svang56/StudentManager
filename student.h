d5f75d355ad7a3bdebe5b54555bdf0a545dc6a10
svang56 <svang56@mail.csuchico.edu>
2018-04-30 17:08:57 -0700

    asdf

---
+++ student.h
@@ -0,0 +1,17 @@
+#ifndef _STUDENT_H_
+#define _STUdenT_H_
+
+class student 
+{
+    private:
+        
+        string firstName;
+        string lastName;
+        
+    public:
+
+    void setName(string, string);
+    void fullName():
+};
+
+#endif
